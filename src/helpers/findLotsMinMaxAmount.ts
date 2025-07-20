9708d495ff756e914e4a147adc12a20f8dc8384a
ik1s3v <ik1s3v@gmail.com>
2025-07-20 21:50:57 +0300

    fix:findLotsMinMaxAmount if amount undefined dropout option size wrong

--- src/helpers/findLotsMinMaxAmount.ts
+++ src/helpers/findLotsMinMaxAmount.ts
@@ -1,7 +1,10 @@
 import type { ILot } from "../../shared/types";
 
 const findLotsMinMaxAmount = (lots: ILot[]) => {
-	const sorted = lots.slice().sort((a, b) => (b.amount ?? 0) - (a.amount ?? 0));
+	const sorted = lots
+		.slice()
+		.filter((lot) => lot.amount)
+		.sort((a, b) => (b.amount ?? 0) - (a.amount ?? 0));
 	return { max: sorted.at(0), min: sorted.at(-1) };
 };
 
